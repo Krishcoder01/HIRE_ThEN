4b06bc5b6543f1e77ab9bb1303c3f99551e38e43
Krishcoder01 <jaink4474@gmail.com>
2025-02-21 13:16:41 +0530

    api

--- Frontend/src/services/Api.jsx
+++ Frontend/src/services/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_URL = 'https://bescanteen.onrender.com';
-const API_URL = "http://localhost:3000/api";
+const API_URL = 'https://hire-then.onrender.com/api';
+// const API_URL = "http://localhost:3000/api";
 
 const instance = axios.create({
   baseURL: API_URL,
